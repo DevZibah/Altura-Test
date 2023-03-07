185ef3152fd685787859fb81a345719668eb5f82
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-03-07 12:13:12 -1100

    Done with all functionalities

--- src/components/NftModal.tsx
+++ src/components/NftModal.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { CgCloseO } from 'react-icons/cg'
+import { Link } from 'react-router-dom'
 
 const NftModal = (props) => {
   if (!props.show) {
     return null
   }
   return (
-    <section className='modal p-4' onClick={props.onClose}>
+    <section className='modal p-4'>
       <div
         className='modal-content p-3 text-center'
         data-aos='zoom-in'
@@ -13,11 +15,14 @@ const NftModal = (props) => {
         data-aos-easing='ease-in-sine'
         data-aos-duration='600'
       >
+        <CgCloseO className='closeicon' onClick={props.onClose} />
         <img src={props.nft.image} alt='' />
         <h4 className='mt-3'>{props.nft.heading}</h4>
         <small>{props.nft.description}</small>
         <small>{props.nft.address}</small>
-        <button className='text-uppercase mt-2 mb-3 nftbutton p-1'>buy nft</button>
+        <button className='text-uppercase mt-2 mb-3 nftbutton p-1'>
+          <Link className='nftlink' to='https://opensea.io/'>buy nft</Link>
+        </button>
       </div>
     </section>
   )
